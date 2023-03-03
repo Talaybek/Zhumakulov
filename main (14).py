8e1689fd36d7174bff4875ca2f5704c727239487
Talaybek <117991012+Talaybek@users.noreply.github.com>
2023-03-03 16:20:27 +0300

    03.03.2023 задание 13

---
+++ main (14).py
@@ -0,0 +1,10 @@
+import math
+a = int(input("Введите число: "))
+b = int(input("Введите число: "))
+if a > 0 and b > 0:
+    z1 = (a + b) /2
+    z2 = a * b
+    n = z2
+    sqrt = math.sqrt(n)
+    print (z1)
+    print (sqrt)
