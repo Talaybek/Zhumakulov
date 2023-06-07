e139cade34be7100d34014e45fc0d706acadb79e
Talaybek <117991012+Talaybek@users.noreply.github.com>
2023-06-07 15:23:59 +0300

    Пр 1- 5 долг за 21.03.2023

---
+++ main (47).py
@@ -0,0 +1,4 @@
+a = int(input())
+b = int(input())
+c = int(input())
+print(a * 0.1, a * 0.01, a * 0.001, sep='\n')
---
+++ main (48).py
@@ -0,0 +1,9 @@
+x = int(input())
+k = int(input())
+if k < x:
+    print(k * x)
+elif k > x:
+    print(k + x)
+else:
+    print(k/x)
+
