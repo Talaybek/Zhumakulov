174b032b480ebc58aa0b647d457ab364ed199dda
Talaybek <117991012+Talaybek@users.noreply.github.com>
2023-06-07 15:45:58 +0300

    Долг за 22 мая 2023 (три загадки)

---
+++ main (64).py
@@ -0,0 +1,22 @@
+print("число от 1 до 10")
+a = int(input())
+while a != 7:
+    print("неверно")
+    a = int(input())
+print("верно")
+
+
+print("число месяца")
+b = int(input())
+while b != 15:
+    print("неверно")
+    b = int(input())
+print("верно")
+
+
+print("творог или творог?")
+c = input()
+while c != "творог" and c != "творог":
+    print("неверно")
+    c = input()
+print("верно")
